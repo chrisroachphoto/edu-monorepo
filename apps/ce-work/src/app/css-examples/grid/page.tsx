694214a29e56a346ac024c44057db30d6fe04a14
Chris Roach <chris.roach@fsgi.com>
2023-11-13 11:04:44 -0600

    new grid example

---
+++ apps/ce-work/src/app/css-examples/grid/page.tsx
@@ -0,0 +1,9 @@
+export default function GridPage(){
+return (    
+<div>
+    <h1>Main Title</h1>
+    <h2>Subheader</h2>
+    <p>Here is a descriptive paragraph tag that tells you some stuff.</p>
+    <div>Additional info</div>
+</div>)
+}
---
+++ apps/ce-work/src/app/css-examples/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css'
+
+export default function CSSExamples (){
+    return (
+       
+            <div>CSS Grid Examples</div>
+        
+    )
+}
