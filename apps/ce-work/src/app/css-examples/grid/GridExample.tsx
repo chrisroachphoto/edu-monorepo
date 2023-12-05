ff1b3006a4fda63602c1f0a1b071cb224dbd5bc5
Chris Roach <chris.roach@fsgi.com>
2023-12-05 10:05:07 -0600

    shaggy arrangement, diagonal bg

---
+++ apps/ce-work/src/app/css-examples/grid/GridExample.tsx
@@ -0,0 +1,23 @@
+import style from "../page.module.css";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(style);
+
+export default function GridExample() {
+  return (
+    <div className={cx("grid-container")}>
+      <div className={cx("grid-item", "beige")} id="grid-1"></div>
+      <div className={cx("grid-item", "diagonal-top")} id="grid-2">
+        <div className={cx("text-overlay")}>Text</div>
+      </div>
+      <div className={cx("grid-item", "diagonal-bottom")} id="grid-3"></div>
+      <div className={cx("grid-item", "beige")} id="grid-4">
+        <div className={cx("text-overlay")}>Text</div>
+      </div>
+      <div className={cx("grid-item", "beige")} id="grid-5"></div>
+      <div className={cx("grid-item", "diagonal-bottom")} id="grid-6">
+        <div className={cx("text-overlay")}>Text</div>
+      </div>
+    </div>
+  );
+}
--- apps/ce-work/src/app/css-examples/grid/page.tsx
+++ apps/ce-work/src/app/css-examples/grid/page.tsx
@@ -1,9 +1,5 @@
-export default function GridPage(){
-return (    
-<div>
-    <h1>Main Title</h1>
-    <h2>Subheader</h2>
-    <p>Here is a descriptive paragraph tag that tells you some stuff.</p>
-    <div>Additional info</div>
-</div>)
+import GridExample from "./GridExample";
+
+export default function GridPage() {
+  return <GridExample />;
 }
